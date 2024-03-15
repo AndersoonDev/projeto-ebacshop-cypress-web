dea35fc85e0fc7a1b5e456eba6aaeaa7e0867ed8
Anderson Silva <andersonsilvadev01@gmail.com>
2024-03-15 16:32:40 -0300

    usando massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,56 +2,59 @@
 
 const perfil = require('../fixtures/perfil.json')
 
-describe('Funcionalidade Login', () => {
+describe('Funcionalidade Login',() => {
 
 
     beforeEach(() => {
         cy.visit('minha-conta')
     });
-        
-  
-    it('Deve fazer login com sucesso', () => {
+
+
+    it('Deve fazer login com sucesso',() => {
         cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste.com')
+        cy.get('#password').type('teste@teste.com',{log: false})
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac')
 
     })
 
-    it('Deve fazer login com sucesso - Usando arquivos de dados', () => {
+    it('Deve fazer login com sucesso - Usando arquivos de dados',() => {
         cy.get('#username').type(perfil.usuario)
-        cy.get('#password').type(perfil.senha)
+        cy.get('#password').type(perfil.senha,{log: false})
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac')
     })
 
-    it.only('Deve fazer login com sucesso - Usando fixture', () => {
-        cy.fixture('perfil').then(dados=>{
+    it('Deve fazer login com sucesso - Usando fixture',() => {
+        cy.fixture('perfil').then(dados => {
             cy.get('#username').type(dados.usuario)
-        cy.get('#password').type(dados.senha, { log: false })
-        cy.get('.woocommerce-form > .button').click()
+            cy.get('#password').type(dados.senha,{log: false})
+            cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac')
+
+
         })
+
     });
 
-    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.get('#username').type('aluno_ebac@teste.com.br')
-        cy.get('#password').type('teste@teste.com')
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido',() => {
+        cy.get('#username').type('alunoebac@teste.com.br')
+        cy.get('#password').type('teste@teste.com',{log: false})
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida',() => {
         cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('00000000000')
+        cy.get('#password').type('00000000000',{log: false})
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('contain','Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
     })
-    
+
 
 })
--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,57 +1,76 @@
 /// <reference types='cypress' />
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
-describe('Funcionalidade Pre Cadastro' , () => {
-
+describe("Funcionalidade Pre Cadastro", () => {
     beforeEach(() => {
-        cy.visit('/minha-conta');
+        cy.visit("/minha-conta");
     });
 
-    it('Deve completar o pre cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type(faker.internet.password())
-        cy.get(':nth-child(4) > .button').click()
+    it("Deve completar o pre cadastro com sucesso", () => {
+        cy.get("#reg_email").type(faker.internet.email());
+        cy.get("#reg_password").type(faker.internet.password());
+        cy.get(":nth-child(4) > .button").click();
 
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.person.firstName())
-        cy.get('#account_last_name').type(faker.person.lastName())
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+        cy.get(
+            ".woocommerce-MyAccount-navigation-link--edit-account > a"
+        ).click();
+        cy.get("#account_first_name").type(faker.person.firstName());
+        cy.get("#account_last_name").type(faker.person.lastName());
+        cy.get(".woocommerce-Button").click();
+        cy.get(".woocommerce-message").should(
+            "contain",
+            "Detalhes da conta modificados com sucesso."
+        );
     });
 
-    it('Deve exibir mensagem de erro ao não inserir o sobrenome', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type('Teste@teste123@')
-        cy.get(':nth-child(4) > .button').click()
-        cy.wait(100)
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.person.firstName())
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-error').should('contain', 'Sobrenome é um campo obrigatório.')
-      })
-       
-    
-      it('Deve exibir mensagem de erro ao não inserir o nome', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type('Teste@teste123@')
-        cy.get(':nth-child(4) > .button').click()
-        cy.wait(100)
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_last_name').type(faker.person.lastName())
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-error').should('contain', 'Nome é um campo obrigatório.')
-      })
-       
-      it('Deve exibir mensagem de erro se não inserir o nome e sobrenome', () => {
-        cy.get('#reg_email').type(faker.internet.email())
-        cy.get('#reg_password').type('Teste@teste123@')
-        cy.get(':nth-child(4) > .button').click()
-        cy.wait(100)
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-error').should('contain', 'Nome é um campo obrigatório.')
-        cy.get('.woocommerce-error').should('contain', 'Sobrenome é um campo obrigatório.')
-      })
-       
-    
-})
+    it("Deve exibir mensagem de erro ao não inserir o sobrenome", () => {
+        cy.get("#reg_email").type(faker.internet.email());
+        cy.get("#reg_password").type("Teste@teste123@");
+        cy.get(":nth-child(4) > .button").click();
+        cy.wait(100);
+        cy.get(
+            ".woocommerce-MyAccount-navigation-link--edit-account > a"
+        ).click();
+        cy.get("#account_first_name").type(faker.person.firstName());
+        cy.get(".woocommerce-Button").click();
+        cy.get(".woocommerce-error").should(
+            "contain",
+            "Sobrenome é um campo obrigatório."
+        );
+    });
+
+    it("Deve exibir mensagem de erro ao não inserir o nome", () => {
+        cy.get("#reg_email").type(faker.internet.email());
+        cy.get("#reg_password").type("Teste@teste123@");
+        cy.get(":nth-child(4) > .button").click();
+        cy.wait(100);
+        cy.get(
+            ".woocommerce-MyAccount-navigation-link--edit-account > a"
+        ).click();
+        cy.get("#account_last_name").type(faker.person.lastName());
+        cy.get(".woocommerce-Button").click();
+        cy.get(".woocommerce-error").should(
+            "contain",
+            "Nome é um campo obrigatório."
+        );
+    });
+
+    it("Deve exibir mensagem de erro se não inserir o nome e sobrenome", () => {
+        cy.get("#reg_email").type(faker.internet.email());
+        cy.get("#reg_password").type("Teste@teste123@");
+        cy.get(":nth-child(4) > .button").click();
+        cy.wait(100);
+        cy.get(
+            ".woocommerce-MyAccount-navigation-link--edit-account > a"
+        ).click();
+        cy.get(".woocommerce-Button").click();
+        cy.get(".woocommerce-error").should(
+            "contain",
+            "Nome é um campo obrigatório."
+        );
+        cy.get(".woocommerce-error").should(
+            "contain",
+            "Sobrenome é um campo obrigatório."
+        );
+    });
+});
