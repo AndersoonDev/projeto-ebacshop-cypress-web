ec9914c8381bf7165b6a17cec35719173b851bb6
Anderson Silva <andersonsilvadev01@gmail.com>
2024-05-14 19:52:45 -0300

    cirrigindo testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-<<<<<<< HEAD
-  e2e: {
-    baseUrl: "http://lojaebac.ebaconline.art.br",
-    setupNodeEvents(on, config) {
-=======
   projectId: 'k2uyor',  
   e2e: {  
     baseUrl: "http://lojaebac.ebaconline.art.br",
     setupNodeEvents(on, config) {    
 
->>>>>>> 5a00b1660872bbfb482e9fc5addebe26c22ae3c2
       // implement node event listeners here
     },
   },
