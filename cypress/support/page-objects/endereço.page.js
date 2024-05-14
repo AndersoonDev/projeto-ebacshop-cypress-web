64b2c26fe6fe974427e5ea2c6003a30370bddc21
Anderson Silva <andersonsilvadev01@gmail.com>
2024-05-14 20:50:39 -0300

    corrigindo testes

--- cypress/support/page-objects/endereço.page.js
+++ cypress/support/page-objects/endereço.page.js
@@ -10,7 +10,7 @@ class EnderecoPage {
         cy.get('#billing_last_name').clear().type(sobrenome)
         cy.get('#billing_company').clear().type(empresa)
         cy.get('#select2-billing_country-container').click().type(pais).click()
-        cy.wait(100)
+        cy.wait(500)
         cy.get('#billing_address_1').clear().type(endereco)
         cy.get('#billing_address_2').clear().type(numero)
         cy.get('#billing_city').clear().type(cidade)
